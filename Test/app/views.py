797068e46bc92a9a254a9e121c0daed45fee9550
quki <quki09@gmail.com>
2016-08-03 12:42:01 +0900

    Add requirements.txt

--- Test/app/views.py
+++ Test/app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpRequest
 
 
-# Create your views here.
 def home(request):
     assert isinstance(request, HttpRequest)
     return render(
